dfe8ef6e2b7de9d256c05fbbb9ba379e383b1458
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-11-13 22:30:32 +0100

    bench win prediction

--- op1/benches/benches.rs
+++ op1/benches/benches.rs
@@ -1,29 +1,32 @@
 use std::hint::black_box;
 
 use criterion::{Criterion, criterion_group, criterion_main};
-use op1::{Tablebase, Value};
+use op1::{Tablebase, Value, guess_winner};
 use shakmaty::{CastlingMode, Chess, fen::Fen};
 
 fn kbpkpppp(c: &mut Criterion) {
-    c.bench_function("kbpkpppp", |b| {
+    let pos: Chess = "8/2b5/8/8/3P4/pPP5/P7/1k2K3 w - - 0 1"
+        .parse::<Fen>()
+        .unwrap()
+        .into_position(CastlingMode::Chess960)
+        .unwrap();
+
+    c.bench_function("probe_kbpkpppp", |b| {
         let mut tablebase = Tablebase::new();
         tablebase.add_path("../tables").unwrap();
 
         b.iter(|| {
             // Test position will cause two table probes. The block size is
             // 635392 bytes. Byte offsets into the block are 20981 and 334346.
-
-            let pos: Chess = black_box("8/2b5/8/8/3P4/pPP5/P7/1k2K3 w - - 0 1")
-                .parse::<Fen>()
-                .unwrap()
-                .into_position(CastlingMode::Chess960)
-                .unwrap();
-
             assert_eq!(
-                tablebase.probe(&pos).unwrap(),
+                tablebase.probe(black_box(&pos)).unwrap(),
                 black_box(Some(Value::Dtc(-1)))
             );
         });
+    });
+
+    c.bench_function("guess_kbpkpppp", |b| {
+        b.iter(|| guess_winner(black_box(&pos)));
     });
 }
 
--- op1/src/lib.rs
+++ op1/src/lib.rs
@@ -3,4 +3,5 @@ mod guess;
 mod table;
 mod tablebase;
 
+pub use guess::guess_winner;
 pub use tablebase::{Tablebase, Value};
