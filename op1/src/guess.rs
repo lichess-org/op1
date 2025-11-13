626698383c7d3f4317d7f5c1fc111f309d2b2a8b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-11-13 22:34:17 +0100

    micro-optimize win prediction performance

--- op1/src/guess.rs
+++ op1/src/guess.rs
@@ -1,32 +1,42 @@
-use shakmaty::{Board, Chess, Color, Position};
+use shakmaty::{Board, Chess, Color, Position, Role};
+
+fn role_value(role: Role) -> i32 {
+    match role {
+        Role::Pawn => 1,
+        Role::Knight => 3,
+        Role::Bishop => 3,
+        Role::Rook => 5,
+        Role::Queen => 9,
+        Role::King => 0,
+    }
+}
 
 fn strength(board: &Board, color: Color) -> i32 {
     let side = board.by_color(color);
     (side & board.pawns()).count() as i32
-        + (side & board.knights()).count() as i32 * 3
-        + (side & board.bishops()).count() as i32 * 3
-        + (side & board.rooks()).count() as i32 * 5
-        + (side & board.queens()).count() as i32 * 9
+        + (side & board.knights()).count() as i32 * role_value(Role::Knight)
+        + (side & board.bishops()).count() as i32 * role_value(Role::Bishop)
+        + (side & board.rooks()).count() as i32 * role_value(Role::Rook)
+        + (side & board.queens()).count() as i32 * role_value(Role::Queen)
 }
 
-fn material_balance(pos: &Chess, depth: u32) -> i32 {
-    if depth == 0 {
-        strength(pos.board(), pos.turn()) - strength(pos.board(), !pos.turn())
-    } else {
-        pos.legal_moves()
-            .into_iter()
-            .map(|m| {
-                let mut after = pos.clone();
-                after.play_unchecked(m);
-                material_balance(&after, depth - 1).saturating_neg()
-            })
-            .max()
-            .unwrap_or(i32::MIN)
-    }
+fn material_balance(pos: &Chess) -> i32 {
+    let current = strength(pos.board(), pos.turn()) - strength(pos.board(), !pos.turn());
+
+    pos.legal_moves()
+        .into_iter()
+        .map(|m| {
+            current
+                + m.promotion()
+                    .map_or(0, |r| role_value(r) - role_value(Role::Pawn))
+                + m.capture().map_or(0, |r| role_value(r))
+        })
+        .max()
+        .unwrap_or(i32::MIN)
 }
 
 pub fn guess_winner(pos: &Chess) -> Color {
-    pos.turn() ^ (material_balance(pos, 1) < 0)
+    pos.turn() ^ (material_balance(pos) < 0)
 }
 
 #[cfg(test)]
@@ -86,7 +96,7 @@ mod tests {
         }
 
         assert!(
-            true_prediction * 100 / total >= 70,
+            true_prediction * 100 / total >= 75,
             "{} %",
             true_prediction * 100 / total
         );
